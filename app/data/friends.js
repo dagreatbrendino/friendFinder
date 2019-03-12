08fea1c7cad200cae9e1a4a14f691fceae3933f6
Brendan K <brendanmkelly94@gmail.com>
2019-03-11 20:36:11 -0500

    the program now actually finds the best match instead of just returning the last person in the people array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,52 @@
-var peopleArray = [];
+var peopleArray = [
+    {
+    "name": "brendan",
+    "photo": "https://staticdelivery.nexusmods.com/mods/1151/images/thumbnails/9605-1-1454866483.jpg",
+    "scores": [
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1"
+    ]
+    },
+    {
+    "name": "brendan1",
+    "photo": "https://wanderinpoet.files.wordpress.com/2018/09/ed-whelan-copy.jpg?w=604&h=270&crop=1",
+    "scores": [
+    "5",
+    "5",
+    "5",
+    "5",
+    "5",
+    "5",
+    "5",
+    "5",
+    "5",
+    "5"
+    ]
+    },
+    {
+    "name": "brendan3",
+    "photo": "https://i.ytimg.com/vi/nsCIcLCOM9E/maxresdefault.jpg",
+    "scores": [
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1"
+    ]
+    }
+    ];
 
 module.exports = peopleArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -16,16 +16,20 @@ module.exports = function(app){
             for(var i =0; i < person.scores.length; i++){
                 //find the difference in answers
                 var questionScore = Math.abs(req.body.scores[i] - person.scores[i]);
+                console.log(questionScore);
                 matchScore += questionScore;
             }
+            console.log(bestMatchScore, " ", matchScore);
             //if the score is lower than the current best friend match, change the match
             if (matchScore < bestMatchScore){
+                bestMatchScore = matchScore;
                 bestMatch = person;
             }
         });
-        
+        //push the user to the friends api so other users can match with them
         potentialFriends.push(req.body);
         console.log(bestMatch);
+        //send the bestMatch object to the user
         res.json(bestMatch);
     })
 }
