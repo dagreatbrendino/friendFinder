15c2ca524b7c1b983df0e913bec4c33d8b7c6c35
Brendan K <brendanmkelly94@gmail.com>
2019-03-11 19:25:55 -0500

    basic front end set up

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,13 @@
+var path = require("path");
+
+module.exports = function(app){
+    //The following routes handle requests from specific url visits, and will
+    //send the user the corresponding HTML page 
+    // app.get("/survey", function(req, res){
+    //     res.sendFile(path.join(__dirname, "../public/survey.html"));
+    // });
+
+    // app.get("*", function(req, res){
+    //     res.sendFile(path.join(__dirname, "../public/home.html"));
+    // })
+}
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-// require("./routes/apiRoutes")(app);
+require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function(){
