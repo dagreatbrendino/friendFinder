1e75f9831228d52d6b3e0159eaefebaf6bdce98f
Brendan K <brendanmkelly94@gmail.com>
2019-03-11 19:58:49 -0500

    added ability to find a best match friend, does not push to front end yet

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,31 @@
-var path = require("path");
+var potentialFriends = require("../data/friends");
 
 module.exports = function(app){
-    //The following routes handle requests from specific url visits, and will
-    //send the user the corresponding HTML page 
-    // app.get("/survey", function(req, res){
-    //     res.sendFile(path.join(__dirname, "../public/survey.html"));
-    // });
 
-    // app.get("*", function(req, res){
-    //     res.sendFile(path.join(__dirname, "../public/home.html"));
-    // })
+    app.get("/api/people", function(req, res){
+        res.json(potentialFriends);
+    })
+
+    app.post("/api/people", function(req, res){
+        var bestMatch;
+        var bestMatchScore = 41;
+        //loop through every person in potential friends
+        potentialFriends.forEach(function(person){
+            var matchScore = 0;
+            //compare the question answers the user submitted to the current potential friend
+            for(var i =0; i < person.scores.length; i++){
+                //find the difference in answers
+                var questionScore = Math.abs(req.body.scores[i] - person.scores[i]);
+                matchScore += questionScore;
+            }
+            //if the score is lower than the current best friend match, change the match
+            if (matchScore < bestMatchScore){
+                bestMatch = person;
+            }
+        });
+        
+        potentialFriends.push(req.body);
+        console.log(bestMatch);
+        res.json(bestMatch);
+    })
 }
